a14d15247e0f37f70c96dfca8347e6918eea59c0
Natalia Lukicheva <natalya.lukicheva@gmail.com>
2021-08-25 19:08:08 +0300

    Added production build of the frontend.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use(express.static('build'))
+
 morgan.token('post-body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
